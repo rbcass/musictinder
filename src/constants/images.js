23937ccd85fb33a97810da189c5b3bcb37f02a3a
rbcass <sodiumhydroxide666@gmail.com>
2023-03-20 23:41:31 +0000

    landing page edit

--- src/constants/images.js
+++ src/constants/images.js
@@ -2,10 +2,14 @@
 
 import jam_match from '../assets/jammatch.png';
 import logo from '../assets/logo.png';
+import guitar from '../assets/guitar.gif';
+
 
 
 
 export default {
   logo,
   jam_match,
+  guitar,
+  
 };
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -4,17 +4,20 @@ import {images} from '../../constants';
 import {headerInfo} from '../../components';
 import './Header.css';
 
+//this element is for the "homepage", we are going to try an implement scroll
 const Header = () => (
   <div className='header' id="home">
-    <div className='headerInfo'>
-
-      {/* <headerInfo/> */}
-      Hey 
-
-    </div>
+    <h1 className='title'>JAM-MATCH</h1>
     <div className='headerImg'>
+      {/* <img src={images.guitar} alt = "guitargif"/> */}
+      
 
     </div>
+    <div className='headerInfo'>
+      <h1 className='subtitle'>One match away from a world tour...</h1>
+      <a href="#"><button className='mainbutt'>Match Me!</button></a>
+
+</div>
   </div>
 );
 
